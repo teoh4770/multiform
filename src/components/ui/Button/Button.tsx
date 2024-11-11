00a0610ee8cb485fe08d0b08d8982492478efccc
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-11-10 19:36:03 -0500

    :lipstick:UI: Button component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,28 @@
+import "./Button.css";
+
+interface IButton {
+  type: "submit" | "reset" | "button" | undefined;
+  variant: "primary" | "secondary" | "confirm";
+  caption: string;
+}
+
+/* 
+Usage:
+1. <Button type="button" variant="secondary" caption="Go Back" />
+2. <Button type="button" variant="primary" caption="Next Step" />
+3. <Button type="button" variant="confirm" caption="Confirm" />
+*/
+
+function Button({
+  variant = "primary",
+  type = "button",
+  caption = "button text",
+}: IButton) {
+  return (
+    <button className="btn" type={type} data-variant={variant}>
+      {caption}
+    </button>
+  );
+}
+
+export { Button };
