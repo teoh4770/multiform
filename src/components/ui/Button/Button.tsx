35c2f22ea7655fa06681d28df4c04357563a6512
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-11-28 11:07:58 -0500

    :sparkles:Add: click handler for button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,15 +4,22 @@ interface IButton {
   type: "submit" | "reset" | "button" | undefined;
   variant: "primary" | "secondary" | "confirm";
   caption: string;
+  clickHandler?: () => void;
 }
 
 function Button({
   variant = "primary",
   type = "button",
   caption = "button text",
+  clickHandler,
 }: IButton) {
   return (
-    <button className={style.button} type={type} data-variant={variant}>
+    <button
+      className={style.button}
+      type={type}
+      data-variant={variant}
+      onClick={clickHandler}
+    >
       {caption}
     </button>
   );
