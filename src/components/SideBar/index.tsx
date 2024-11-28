4ad07606903cd72af4d42350161b2cd9790bd4cd
Chee Kian Teoh <teoh4770@mylaurier.ca>
2024-11-28 11:21:53 -0500

    :bug:Fix: Sidebar As UI component only - make sidebar to be a pure presentational component, didn't have knowledge about the other components within it like steps components

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,29 +1,16 @@
-// container components that holds a list of steps?
-// position will change depends on the viewport?
-// mobile view (top, just step indicators)
-// tablet view / desktop view (side, step indicators and their detail)
-
-// sidebar shouldn't know about steps
-// just a container
-// the container
-import { Steps } from "../Steps";
+import { ReactNode } from "react";
 import style from "./SideBar.module.css";
 
-type PaymentFormStep = "Your Info" | "Select Plan" | "Add-Ons" | "Summary";
+interface ISidebar {
+  children?: ReactNode;
+}
 
-const steps: PaymentFormStep[] = [
-  "Your Info",
-  "Select Plan",
-  "Add-Ons",
-  "Summary",
-];
-
-function SideBar() {
+function Sidebar({ children }: ISidebar) {
   return (
     <aside className={style.sideBar}>
-      <Steps stepList={steps} currentStep={2} />
-    </aside>
+      {children}
+      </aside>
   );
 }
 
-export { SideBar };
+export { Sidebar };
